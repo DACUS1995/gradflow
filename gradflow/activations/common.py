0e068b7bfbbb2c355be9f530a3cec2165a11f350
DACUS1995 <studormarian@gmail.com>
2022-11-30 12:05:17 +0200

    added  sigmoid activation

--- gradflow/activations/common.py
+++ gradflow/activations/common.py
@@ -21,9 +21,53 @@ def relu(input: Variable) -> Variable:
 
 
 def softmax(input: Variable) -> Variable:
+    if not isinstance(input.data, NumpyDataContainer):
+        raise ValueError("Only supported data container is numy based curently.")
+    
     e_x = (input.data - input.data.max()).exp()
     result = e_x.data / e_x.data.sum(axis=0)
-    return result
+    variable = Variable(result, parents=(input, ))
+
+    if input.requires_grad:
+        def _back_grad_fn():
+            raise NotImplemented()
+        variable._back_grad_fn = _back_grad_fn
+        variable.requires_grad = True
+
+    return variable
 
     # SM = self.value.reshape((-1,1))
     # jac = np.diagflat(self.value) - np.dot(SM, SM.T)
+
+
+def sigmoid(input: Variable) -> Variable:
+    if not isinstance(input.data, NumpyDataContainer):
+        raise ValueError("Only supported data container is numy based curently.")
+
+    result = 1 / (1 + np.exp(-input.data.data))
+    variable = Variable(result, parents=(input, ))
+
+    if input.requires_grad:
+        def _back_grad_fn():
+            # deriv_sigmoid = sigmoid(x) * (1 - sigmoid(x))
+            input.grad += (result.data * (1 - result)) * variable.grad
+        variable._back_grad_fn = _back_grad_fn
+        variable.requires_grad = True
+
+    return variable
+
+
+def tanh(input: Variable) -> Variable:
+    if not isinstance(input.data, NumpyDataContainer):
+        raise ValueError("Only supported data container is numy based curently.")
+    
+    result = 2 / (1 + (-2 * input.data).exp()) - 1
+    variable = Variable(result, parents=(input, ))
+
+    if input.requires_grad:
+        def _back_grad_fn():
+            input.grad += (1 - result.data ** 2) * variable.grad
+        variable._back_grad_fn = _back_grad_fn
+        variable.requires_grad = True
+
+    return variable
